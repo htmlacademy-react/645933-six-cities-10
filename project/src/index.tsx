c5c2b3bceae5d2b57017d04bb79ef3f34b94864d
Заур <zaurinio@inbox.ru>
2022-07-16 22:50:02 +0300

    Начинаем создавать компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  OFFERS_NUMBER: 312,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      offersNumber={Setting.OFFERS_NUMBER}
+    />
   </React.StrictMode>,
 );
